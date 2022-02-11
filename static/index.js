9902528dacb8a2c95dc1d9cab6bf9ec3f81bf970
paomianzing <99480474+paomianzing@users.noreply.github.com>
2022-02-11 20:49:06 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '才几天手生成这样啊';
+        if (cps <= 8) return '再多来几个';
+        if (cps <= 10)  return '呦西！';
+        if (cps <= 15) return '教我';
         return '人？';
     }
 
